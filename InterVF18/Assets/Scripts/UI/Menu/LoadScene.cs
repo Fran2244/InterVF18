cfecaf67aa032ce62bb38de4e8aa0f352856f0cd
thavide <thavide@gmail.com>
2018-01-17 17:52:33 -0500

    Scene Loading

--- InterVF18/Assets/Scripts/UI/Menu/LoadScene.cs
+++ InterVF18/Assets/Scripts/UI/Menu/LoadScene.cs
@@ -13,20 +13,46 @@ public class LoadScene : MonoBehaviour {
     int sceneIndex;
 
     [SerializeField]
-    Image imgFader;
+    CanvasGroup imgFader;
+
+    [SerializeField]
+    CanvasGroup[] menuItems;
+
+    GameObject rootObj;
 
     public void Load()
     {
-        #if (UNITY_EDITOR)
-                EditorSceneManager.LoadScene(sceneIndex);
-        #else
-                SceneManager.LoadScene(sceneIndex);
-        #endif
+        Transform root = transform.root;
+        rootObj = root.gameObject;
+        DontDestroyOnLoad(rootObj);
+        StartCoroutine(LoadSceneFade());
     }
 
-    //IEnumerator LoadSceneFade()
-    //{
-
-    //}
+    IEnumerator LoadSceneFade()
+    {
+        float startTime = Time.time;
+        imgFader.alpha = 0f;
+        while (imgFader.alpha < 1.0f)
+        {
+            imgFader.alpha = Mathf.Lerp(0f, 1f, Time.time - startTime);
+            yield return null;
+        }
+#if (UNITY_EDITOR)
+        EditorSceneManager.LoadScene(sceneIndex);
+#else
+                SceneManager.LoadScene(sceneIndex);
+#endif
+        foreach(CanvasGroup obj in menuItems)
+        {
+            obj.alpha = 0f;
+        }
+        startTime = Time.time;
+        while (imgFader.alpha > 0.0f)
+        {
+            imgFader.alpha = Mathf.Lerp(1f, 0f, Time.time - startTime);
+            yield return null;
+        }
+        Destroy(rootObj);
+    }
 
 }
