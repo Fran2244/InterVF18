e6a9ff3e683c484cc17959d7ec1ffdd9e7e40739
thavide <thavide@gmail.com>
2018-01-17 16:40:08 -0500

    Added button visual to menu

---
+++ InterVF18/Assets/Scripts/UI/Menu/LoadScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if (UNITY_EDITOR)
+using UnityEditor.SceneManagement;
+#endif
+
+public class LoadScene : MonoBehaviour {
+
+    [SerializeField]
+    int sceneIndex;
+
+    public void Load()
+    {
+        #if (UNITY_EDITOR)
+                EditorSceneManager.LoadScene(sceneIndex);
+        #else
+                SceneManager.LoadScene(sceneIndex);
+        #endif
+    }
+}
