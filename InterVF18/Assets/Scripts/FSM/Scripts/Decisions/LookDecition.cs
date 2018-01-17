4a6331306bb27eb43dcea1045291ae57e5cb81cd
Vitalie_Notebook <vitalie.jurjiu@gmail.com>
2018-01-17 16:06:52 -0500

    + Added tag Enemy

---
+++ InterVF18/Assets/Scripts/FSM/Scripts/Decisions/LookDecition.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "AI/Decisions/Look")]
+public class LookDecition : Decision
+{
+    public override bool Decide(StateController controller)
+    {
+        RaycastHit hit;
+
+        Debug.DrawRay(controller.eye.position, controller.eye.forward.normalized * 3.0f, Color.green);
+
+        if (Physics.SphereCast(controller.eye.position,
+                              0.3f,
+                              controller.eye.forward,
+                              out hit,
+                              3.0f)
+           && hit.collider.CompareTag("Enemy"))
+        {
+            controller.target = hit.transform;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
