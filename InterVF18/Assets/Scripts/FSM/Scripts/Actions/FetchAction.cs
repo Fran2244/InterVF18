0e1f9521cc88c13ec35d53363030cbeafbb9294d
Vitalie_Notebook <vitalie.jurjiu@gmail.com>
2018-01-17 13:40:26 -0500

    * Changed Gaurd

--- InterVF18/Assets/Scripts/FSM/Scripts/Actions/FetchAction.cs
+++ InterVF18/Assets/Scripts/FSM/Scripts/Actions/FetchAction.cs
@@ -11,7 +11,9 @@ public class FetchAction : Action
             targetState.currentState = null;
             targetState.navMeshAgent.enabled = false;
             controller.fetchObj = controller.target.gameObject;
+            controller.fetchObj.GetComponent<Collider>().enabled = false;
             controller.fetchObj.transform.SetParent(controller.gameObject.transform);
+            controller.fetchObj.transform.localPosition = new Vector3(0.0f, 0.5f, 2.2f); 
 
             controller.target = VisibilityManager.Instance.prisonPos;
             controller.navMeshAgent.speed = controller.navMeshAgent.speed * 2;
@@ -34,7 +36,13 @@ public class FetchAction : Action
         if (controller.target != null)
         {
             controller.navMeshAgent.speed = controller.navMeshAgent.speed / 2;
-            controller.fetchObj.transform.SetParent(controller.target.gameObject.transform);
+            //controller.fetchObj.transform.SetParent(controller.target.gameObject.transform);
+            if (ObjectManager.Instance.Enemies.Remove(controller.fetchObj))
+            {
+                Destroy(controller.fetchObj, 1.0f);
+            }
         }
+
+        controller.isChasing = false;
     }
 }
