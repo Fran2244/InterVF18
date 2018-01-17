2c5a07b99564af3ba2e7f0f0a74c0494de408c5b
thavide <thavide@gmail.com>
2018-01-16 23:12:20 -0500

    Detection Scripts

---
+++ Script/TempAddToCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempAddToCollection : MonoBehaviour {
+    enum ObjectType {Enemy, Detector, Guard };
+
+    [SerializeField]
+    ObjectType type;
+
+    // Use this for initialization
+    void Start () {
+        switch (type)
+        {
+            case ObjectType.Enemy:
+                ObjectManager.Instance.AddEnemy(gameObject);
+                break;
+            case ObjectType.Detector:
+                ObjectManager.Instance.AddDetector(gameObject);
+                break;
+            case ObjectType.Guard:
+                ObjectManager.Instance.AddGuard(gameObject);
+                break;
+            default:
+                break;
+        }
+    }
+}
