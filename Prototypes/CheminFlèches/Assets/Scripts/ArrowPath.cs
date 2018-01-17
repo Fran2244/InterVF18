3521821aef29f79093abc9d220bc1c86de0a21a7
Francis <xxx@xyz.com>
2018-01-17 01:35:28 -0500

    +Flèches pour chemin avec un prototype d'exemple

---
+++ Prototypes/CheminFlèches/Assets/Scripts/ArrowPath.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ArrowPath : MonoBehaviour {
+
+    [SerializeField] float scrollSpeed = 0.5f;
+
+    Vector2 offset = new Vector2(0, 0);
+
+    Material mat;
+
+	void Start () {
+        LineRenderer line = GetComponent<LineRenderer>();
+        line.positionCount = transform.childCount;
+        for (int i=0;i< transform.childCount; i++) {
+            line.SetPosition(i, transform.GetChild(i).transform.position);
+        }
+        mat = line.material;
+    }
+	
+	void Update () {
+        offset.x -= Time.deltaTime * scrollSpeed;
+
+        mat.SetTextureOffset("_MainTex", offset);
+
+    }
+}
