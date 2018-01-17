322bb575a346838ac7c68f4a1efd8790b5947985
Dmitri Azimov <dmitri.azimov@gmail.com>
2018-01-17 00:52:31 -0500

    Reorganised everything

---
+++ InterVF18/Assets/Scripts/LineOfSight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineOfSight : MonoBehaviour {
+
+    [SerializeField]
+    float viewAngle;
+
+    [SerializeField]
+    float viewDistance;
+
+    public float ViewAngle
+    {
+        get
+        {
+            return viewAngle;
+        }
+    }
+    
+    public float ViewDistance
+    {
+        get
+        {
+            return viewDistance;
+        }
+    }
+
+
+}
