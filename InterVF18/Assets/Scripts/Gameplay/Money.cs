10484e5fb7c19293afec04342fbcb50e2f2f989c
Francis <xxx@xyz.com>
2018-01-18 05:45:33 -0500

    *money class improved

--- InterVF18/Assets/Scripts/Gameplay/Money.cs
+++ InterVF18/Assets/Scripts/Gameplay/Money.cs
@@ -1,7 +1,18 @@
 ﻿public class Money {
+    ///// Constants /////
+    const int moneyStart = 50;
+    const int priceCamera = 10;
+    const int priceCameraIncrease = 1;
+    const int priceGuard = 10;
+    const int priceGuardIncrease = 1;
+    const int priceLaser = 10;
+    const int priceLaserIncrease = 1;
 
     ///// Variables /////
-    int _money = 50;
+    int _money = moneyStart;
+    int _priceCamera = priceCamera;
+    int _priceGuard = priceGuard;
+    int _priceLaser = priceLaser;
 
     ///// Singleton /////
     private Money() {
@@ -53,4 +64,40 @@
         } else
             return false;
     }
+
+    ///// Reset /////
+
+    static public void reset() {
+        Singleton._reset();
+    }
+
+    void _reset() {
+        _money = moneyStart;
+        _priceCamera = priceCamera;
+        _priceGuard = priceGuard;
+        _priceLaser = priceLaser;
+
+    }
+
+    ///// Camera /////
+    static public int cameraPrice { get { return Singleton._priceCamera; } }
+    static bool cameraTestBuy() { return Singleton._cameraTestBuy(); }
+    bool _cameraTestBuy() { return _TestBuy(_priceCamera); }
+    static bool cameraBuy() { return Singleton._cameraBuy(); }
+    bool _cameraBuy() { return _Buy(_priceCamera); }
+
+    ///// Guard /////
+    static public int guardPrice { get { return Singleton._priceGuard; } }
+    static bool guardTestBuy() { return Singleton._guardTestBuy(); }
+    bool _guardTestBuy() { return _TestBuy(_priceGuard); }
+    static bool guardBuy() { return Singleton._guardBuy(); }
+    bool _guardBuy() { return _Buy(_priceGuard); }
+
+    ///// Laser /////
+    static public int laserPrice { get { return Singleton._priceLaser; } }
+    static bool laserTestBuy() { return Singleton._laserTestBuy(); }
+    bool _laserTestBuy() { return _TestBuy(_priceLaser); }
+    static bool laserBuy() { return Singleton._laserBuy(); }
+    bool _laserBuy() { return _Buy(_priceLaser); }
+
 }
