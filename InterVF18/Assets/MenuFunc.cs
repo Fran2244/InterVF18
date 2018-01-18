5bb4edb123b5ed72dd6236ed48b1466a04462f36
Dmitri Azimov <dmitri.azimov@gmail.com>
2018-01-18 09:02:13 -0500

    menu sakfdhsdiufhsdjos

---
+++ InterVF18/Assets/MenuFunc.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class MenuFunc : MonoBehaviour {
+
+    public GameObject credits;
+
+    public void ToggleCredits()
+    {
+        credits.SetActive(!credits.activeSelf);
+    }
+}
