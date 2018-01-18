b80549821c3072657b6d8a29e07bce3ad9f16ed9
Dmitri Azimov <dmitri.azimov@gmail.com>
2018-01-18 00:28:47 -0500

    prison icon

---
+++ InterVF18/Assets/Scripts/ObjectOfInterest/PrisonIcon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class PrisonIcon : MonoBehaviour {
+
+    [SerializeField] GameObject icon;
+    [SerializeField] Light bgLight;
+    Transform cam;
+    float sinMultiplier = 1f;
+    float lightMultiplier = 1f;
+    float originalVetical; 
+	
+    void Start()
+    {
+        originalVetical = icon.transform.position.y;
+        Transform cam = Camera.main.transform;
+    }
+
+	void Update ()
+    {
+        icon.transform.position = new Vector3(icon.transform.position.x, originalVetical += Mathf.Sin(Time.deltaTime) * sinMultiplier, icon.transform.position.z);
+        bgLight.intensity += Mathf.Sin(Time.deltaTime) * lightMultiplier;
+        icon.transform.LookAt(cam.position);
+	}
+}
